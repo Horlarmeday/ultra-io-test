e9b882c1b39fad361f8ec7b8bb4a109eefb52c3f
Mahmud <ajaomahmud@gmail.com>
2022-06-14 11:27:09 +0100

    chore: Use response interceptors globally

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import * as bodyParser from 'body-parser';
 import * as morgan from 'morgan';
 import helmet from 'helmet';
 import { Logger, LoggerService } from '@nestjs/common';
+import { ValidateInputPipe } from './core/pipes/validate.pipes';
+import { ResponseInterceptor } from './core/interceptors/response.interceptors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,6 +16,10 @@ async function bootstrap() {
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use(morgan('dev'));
   app.setGlobalPrefix('api');
+
+  // handle all user input validation globally
+  app.useGlobalPipes(new ValidateInputPipe());
+  app.useGlobalInterceptors(new ResponseInterceptor());
 
   await app.listen(port, () => {
     logger.log(`Server running on port ${port}`);
