2dd04532f68431a4532cd63e14e73655410e4d1f
Mahmud <ajaomahmud@gmail.com>
2022-06-14 11:12:53 +0100

    feat: Add response messages enum

---
+++ src/core/responses/message.responses.ts
@@ -0,0 +1,8 @@
+export enum Messages {
+  CREATED = 'Data created successfully',
+  RETRIEVED = 'Data retrieved',
+  DELETED = 'Data deleted successfully',
+  UPDATED = 'Data updated successfully',
+  NOT_FOUND = 'Data not found',
+  PROCESSED = 'Data processed',
+}
