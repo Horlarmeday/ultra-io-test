9675288052484039cd9ba9edc97e1476fe9a1783
Mahmud <ajaomahmud@gmail.com>
2022-06-14 11:16:33 +0100

    feat: Add app constants

---
+++ src/core/constants/index.ts
@@ -0,0 +1,8 @@
+export const SEQUELIZE = 'SEQUELIZE';
+export const DEVELOPMENT = 'development';
+export const TEST = 'test';
+export const PRODUCTION = 'production';
+export const CASCADE = 'CASCADE';
+export const SUCCESS = 'success';
+export const PROCESS_GAMES = 'process-games';
+export const APPROVED = 'approved';
