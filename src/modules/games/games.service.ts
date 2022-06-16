1c191b9888ad6bf2058e4f76085c2d5fe03e0f9f
Mahmud <ajaomahmud@gmail.com>
2022-06-16 23:12:12 +0100

    chore: Modify create game to use transaction

--- src/modules/games/games.service.ts
+++ src/modules/games/games.service.ts
@@ -1,4 +1,10 @@
-import { Injectable, Logger, NotFoundException } from '@nestjs/common';
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  Logger,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateGameDto } from './dto/create-game.dto';
 import { UpdateGameDto } from './dto/update-game.dto';
 import { InjectModel } from '@nestjs/sequelize';
@@ -8,18 +14,40 @@ import { QueryParamsDto } from '../../common/dto/queryParams.dto';
 import { Publisher } from './entities/publisher.entity';
 import { Messages } from '../../core/responses/message.responses';
 import { GameJobService } from '../../core/jobs/game/game-job.producer';
+import { Sequelize } from 'sequelize-typescript';
 
 @Injectable()
 export class GamesService {
   private readonly logger = new Logger(GamesService.name);
   constructor(
     @InjectModel(Game) private gamesRepository: typeof Game,
+    @InjectModel(Publisher) private publishersRepository: typeof Publisher,
     private readonly generalHelpers: GeneralHelpers,
     private readonly gameJobService: GameJobService,
+    private sequelize: Sequelize,
   ) {}
 
   async create(createGameDto: CreateGameDto): Promise<Game> {
-    return await this.gamesRepository.create<Game>({ ...createGameDto });
+    const { name, phone, siret, price, release_date, tags, title } =
+      createGameDto;
+    try {
+      return await this.sequelize.transaction(async (t) => {
+        const transactionHost = { transaction: t };
+
+        const publisher = await this.publishersRepository.create<Publisher>(
+          { name, phone, siret },
+          transactionHost,
+        );
+        this.logger.log(`Publisher ${publisher.id} created successfully`);
+        return await this.gamesRepository.create<Game>(
+          { price, release_date, tags, title, publisher_id: publisher.id },
+          transactionHost,
+        );
+      });
+    } catch (err) {
+      this.logger.error(err);
+      throw new HttpException(err.message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 
   async findAll(queryDto: QueryParamsDto): Promise<{
@@ -41,14 +69,8 @@ export class GamesService {
     return this.generalHelpers.paginate(applications, currentPage, limit);
   }
 
-  async findOne(id: string): Promise<Game> {
-    const game = await this.gamesRepository.findOne<Game>({ where: { id } });
-    if (!game) throw new NotFoundException(Messages.NOT_FOUND);
-    return game;
-  }
-
   async fetchPublisher(id: string) {
-    return await this.gamesRepository.findOne<Game>({
+    const game = await this.gamesRepository.findOne<Game>({
       where: { id },
       include: [
         {
@@ -56,13 +78,20 @@ export class GamesService {
         },
       ],
     });
+    if (!game) throw new NotFoundException(Messages.NOT_FOUND);
+    return game;
   }
 
   async update(id: string, updateGameDto: UpdateGameDto): Promise<unknown> {
-    return this.gamesRepository.update<Game>(
-      { ...updateGameDto },
-      { where: { id } },
-    );
+    const game = await this.gamesRepository.update<Game>(updateGameDto, {
+      where: { id },
+      returning: true,
+    });
+    return GamesService.restructureGameResponse(game);
+  }
+
+  private static restructureGameResponse(game: [number, Game[]]) {
+    return game?.[1]?.[0];
   }
 
   async delete(id: string): Promise<number> {
@@ -84,11 +113,6 @@ export class GamesService {
     return await this.gamesRepository.findAndCountAll<Game>({
       limit: limit,
       offset: offset,
-      include: [
-        {
-          model: Publisher,
-        },
-      ],
     });
   }
 }
